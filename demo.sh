438dafe488fe49c119b694996812133d8ef45bb5
diqiuxin <501993589@qq.com>
2017-11-14 12:11:04 +0800

    demo.sh脚本提交-第一次

---
+++ demo.sh
@@ -0,0 +1,25 @@
+
+if [ -d '$2']; then
+  cd $2
+else
+  cd ~/Desktop   #默认创建文件夹的路径 ~/Desktop
+fi
+
+if [ -d $1 ]; then
+  echo "$1 文件夹已经存在"
+  exit 1
+else
+  #cd ~/Desktop
+  mkdir $1
+  cd $1
+  mkdir css js
+  #touch index.html css/style.css js/main.js
+  echo "<!DOCTYPE>
+  <title>Hello</title>
+  <h1>Hi</h1>" > index.html
+  echo "h1{color: red;}" > css/style.css
+  echo "var string = 'Hello World';
+  alert(string);" > js/main.js
+  echo 'create $1 dir success :)'
+  exit 0
+fi
