612f2c4a5b2e2535e9a76739b7042146e41f6997
diqiuxin <501993589@qq.com>
2017-11-14 12:45:57 +0800

    改进demo.sh 添加demo-0.sh备用

--- demo.sh
+++ demo.sh
@@ -1,25 +1,36 @@
-
-if [ -d '$2']; then
-  cd $2
+if [ -z $1 ]; then
+  echo '请输入文件名'
 else
-  cd ~/Desktop   #默认创建文件夹的路径 ~/Desktop
-fi
 
-if [ -d $1 ]; then
-  echo "$1 文件夹已经存在"
-  exit 1
-else
-  #cd ~/Desktop
-  mkdir $1
-  cd $1
-  mkdir css js
-  #touch index.html css/style.css js/main.js
-  echo "<!DOCTYPE>
-  <title>Hello</title>
-  <h1>Hi</h1>" > index.html
-  echo "h1{color: red;}" > css/style.css
-  echo "var string = 'Hello World';
-  alert(string);" > js/main.js
-  echo 'create $1 dir success :)'
-  exit 0
+  if [ -z $2 ]; then
+    cd ~/Desktop/
+    echo '文件创建路径为：'
+    pwd
+    #sh demo-0.sh $1
+  else
+    cd $2
+    echo '文件创建路径为：'
+    pwd
+    #sh demo-0.sh $1
+  fi
+
+  if [ -d $1 ]; then
+    echo "$1 文件夹已经存在"
+    exit 1
+  else
+    #cd ~/Desktop
+    mkdir $1
+    cd $1
+    mkdir css js
+    #touch index.html css/style.css js/main.js
+    echo "<!DOCTYPE>
+    <title>Hello</title>
+    <h1>Hi</h1>" > index.html
+    echo "h1{color: red;}" > css/style.css
+    echo "var string = 'Hello World';
+    alert(string);" > js/main.js
+    echo 'create $1 dir success :)'
+    exit 0
+  fi
+
 fi
