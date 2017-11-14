d16b847c44a6aa1e305bf81f3ab14f095fa5f356
diqiuxin <501993589@qq.com>
2017-11-14 20:07:44 +0800

    改进html、js文件的换行问题

--- demo.sh
+++ demo.sh
@@ -8,12 +8,12 @@ else                                       # $1 不存在时，执行
   mkdir $1
   cd $1
   mkdir css js                             #以下为index.html、style.css、mian.js各文件内容
-  echo "<!DOCTYPE>                          
-  <title>Hello</title>
-  <h1>Hi</h1>" > index.html
+  echo "<!DOCTYPE>
+<title>Hello</title>
+<h1>Hi</h1>" > index.html
   echo "h1{color: red;}" > css/style.css
   echo "var string = 'Hello World';
-  alert(string);" > js/main.js
+alert(string);" > js/main.js
   echo 'create $1 dir success :)'
   exit 0
 fi
